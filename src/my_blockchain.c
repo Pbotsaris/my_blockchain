ddc64036a858638cda63d37867dcee33632c3255
Khalil <khalil@pop-os.localdomain>
2021-07-22 17:05:00 +0300

    comminted most of the main

--- src/my_blockchain.c
+++ src/my_blockchain.c
@@ -23,64 +23,67 @@
 int main(void)
 {
     /* Init list with 1 items. List functions ALWAYS return the head */
-    node_t *head = init_list("cat", 0); 
+    /* node_t *head = init_list("cat", 0); */
 
-    int i;
+    int input = STDIN_FILENO;
+    input_split *processed_input = process_input(input);
 
-    for (i = 1; i < 5; ++i) 
-        head = add_node(head, "dog", i);
 
-    for (i = 5; i < 10; ++i) 
-        head = add_node(head, "sheep", i);
+    /* int i; */
 
-    printf("------ Complete List ------\n");
-    print_list(head);
+    /* for (i = 1; i < 5; ++i) */ 
+    /*     head = add_node(head, "dog", i); */
 
-    printf("------ Print only dog------\n");
+    /* for (i = 5; i < 10; ++i) */ 
+    /*     head = add_node(head, "sheep", i); */
 
-    print_block_list(head, "dog");
+    /* printf("------ Complete List ------\n"); */
+    /* print_list(head); */
 
-    printf("------ Remove node: 1 Cat and 1 Dog ------\n");
-    /* this removes a node by the nid one at time. */
-    head = remove_node(head, 4);
-    head = remove_node(head, 0);
-    print_list(head);
+    /* printf("------ Print only dog------\n"); */
 
-    /* add a block to a given node */
-    printf("------ Add Block: Giraffe  ------\n");
-    char* buffer = malloc(sizeof(char)*10);
-    /* char str[10] = "giraffe"; */
-    strcpy(buffer, "giraffe"); 
-    head = add_block(head, buffer, 7);
-    print_list(head);
+    /* print_block_list(head, "dog"); */
 
-    printf("------ Remove block: Dogs ------\n");
-    /* this will remove all nodes with a matching bid */
-    head = remove_block(head, "sheep");
-    head = remove_block(head, "dog");
-    print_list(head);
+    /* printf("------ Remove node: 1 Cat and 1 Dog ------\n"); */
+    /* /1* this removes a node by the nid one at time. *1/ */
+    /* head = remove_node(head, 4); */
+    /* head = remove_node(head, 0); */
+    /* print_list(head); */
 
-    printf("------ Check if node exists ------\n");
-    int pos;
+    /* /1* add a block to a given node *1/ */
+    /* printf("------ Add Block: Giraffe  ------\n"); */
+    /* /1* char* buffer = malloc(sizeof(char)*10); *1/ */
+    /* /1* /2* char str[10] = "giraffe"; *2/ *1/ */
+    /* /1* strcpy(buffer, "giraffe"); *1/ */ 
+    /* head = add_block(head, "giraffe", 7); */
+    /* print_list(head); */
 
-    if((pos = node_exists(head, 5)) >= 0)
-        printf("\nNode 8 exists at pos %i in the list\n", pos);
+    /* printf("------ Remove block: Dogs ------\n"); */
+    /* /1* this will remove all nodes with a matching bid *1/ */
+    /* head = remove_block(head, "sheep"); */
+    /* head = remove_block(head, "dog"); */
+    /* print_list(head); */
 
-    if((pos = node_exists(head, 100)) < 0)
-        printf("Node 100 does not exists\n\n");
+    /* printf("------ Check if node exists ------\n"); */
+    /* int pos; */
 
-    printf("------ Check if block exists ------\n");
+    /* if((pos = node_exists(head, 5)) >= 0) */
+    /*     printf("\nNode 8 exists at pos %i in the list\n", pos); */
 
-    if((pos = block_exists(head, "sheep")) >= 0)
-        printf("\nNode sheep exists. First match at %i\n", pos);
+    /* if((pos = node_exists(head, 100)) < 0) */
+    /*     printf("Node 100 does not exists\n\n"); */
+
+    /* printf("------ Check if block exists ------\n"); */
+
+    /* if((pos = block_exists(head, "sheep")) >= 0) */
+    /*     printf("\nNode sheep exists. First match at %i\n", pos); */
     
-    if((pos = block_exists(head, "elephant")) < 0)
-        printf("Block elephant does not exists\n\n");
+    /* if((pos = block_exists(head, "elephant")) < 0) */
+    /*     printf("Block elephant does not exists\n\n"); */
     
-    printf("------ Final List ------\n");
-    print_list(head);
-    free_list(head);
-
+    /* printf("------ Final List ------\n"); */
+    /* print_list(head); */
+    /* free_list(head); */
 
     return 0;
 }
