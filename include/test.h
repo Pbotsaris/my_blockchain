f582ca60f24f8fc841f6f489014bbbd312b4060f
Khalil <khalil@pop-os.localdomain>
2021-08-01 20:15:52 +0300

    Changed a name of the funtion get_option

--- include/test.h
+++ include/test.h
@@ -1,14 +1,13 @@
 #ifndef TEST_H
 #define TEST_H
 #include "./list.h"
-
 char *get_input(char *input, int *input_index);
-option_t get_option(input_t *input);
+option_t basic_commands(char *buffer);
 char *clean_std_in(char *std_in);
 bool_t check_block_impact(char *std_in);
-status_t check_add_block(input_t *input, char* std_in, unsynced *data);
+status_t check_add_block(input_t *input, char* std_in, node_t *data);
 bool_t check_number(char* input);
-status_t check_add_node(input_t *input, char *std_in, unsynced *data);
+status_t check_add_node(input_t *input, char *std_in, node_t *data);
 
 #endif
 
