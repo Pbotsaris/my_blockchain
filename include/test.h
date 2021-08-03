228d9862693a1f431b0fb3b2ddfec002046a505a
Pedro Botsaris <pedro@antfood.com>
2021-08-03 10:49:46 +0200

    fixed test.h func declaratons

--- include/test.h
+++ include/test.h
@@ -10,11 +10,11 @@ bool_t check_number(char* input);
 option_t process_input(int std_in, char *buffer);
 status_t parse_input(input_t *input, char *buffer);
 
-node_t *check_add_node(input_t *input, node_t *unsynced);
-node_t *check_add_block(input_t *input, node_t *unsynced);
+node_t *check_add_node(input_t *input, node_t *head);
+node_t *check_add_block(input_t *input, node_t *head);
 
-node_t *check_rm_node(input_t *input, node_t *unsynced);
-node_t *check_rm_block(input_t *input, node_t *unsynced);
+node_t *check_rm_node(input_t *input, node_t *head);
+node_t *check_rm_block(input_t *input, node_t *head);
 
 node_t *process_commands(char *buffer);
 
