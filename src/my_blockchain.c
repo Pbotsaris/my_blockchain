fa77c9fa247620f58a052e96059bff8d2cf702e7
khalilmasri <khalil.1996km@gmail.com>
2021-08-03 15:35:52 +0300

    added prompt and free

--- src/my_blockchain.c
+++ src/my_blockchain.c
@@ -19,21 +19,31 @@
 #include <stdlib.h>
 #include  <stdio.h>
 #include "../include/list.h"
+void free_struct(input_t *input){
+
+    free(input->bid);
+    free(input->buffer);
+    free(input->cmd);
+    free(input->typ);
+    free(input->one_time_bid);
+    free(input->nid);
+    free(input->unsynced);
+}
 
 int main(void)
 {
-    node_t *unsynced = NULL;
-    while(process_input(STDIN_FILENO, unsynced) != QUIT){
 
+    input_t *input = malloc(sizeof(input_t));
+    input->option = NONE;
+    input->unsynced = NULL;
+    
+    printf("Program starting...\n");
+    while(input->option != QUIT){
+        prompt_display(input->option);
+        input->option = process_input(STDIN_FILENO, input);
     } 
-    /* printf("cmd: %s, type: %s, bid: %s, nid: %i\n", input->cmd, input->type, input->bid, input->nid); */
 
-    /* TODO: NEED FUNCTION TO FREE THESE */
- //   free(input->cmd);
-  //  free(input->type);
-  //  if(input->bid != NULL)
-   //     free(input->bid);
-
-
+    free_struct(input);
+    free(input);
     return 0;
 }
