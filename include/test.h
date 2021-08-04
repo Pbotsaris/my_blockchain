1822c018e9fce71444a733b985161fa74e51d098
Pedro Botsaris <pedro@antfood.com>
2021-08-04 12:57:37 +0200

    fixed test.h with process_input and process_command new declarations

--- include/test.h
+++ include/test.h
@@ -1,22 +1,29 @@
 #ifndef TEST_H
 #define TEST_H
 #include "./my_blockchain.h"
-char *get_input(char *input, int *input_index);
+
+
+/* HELPER process_input */
 option_t basic_commands(char *buffer);
+
+/* HELPER process_commands */
 char *clean_std_in(char *std_in);
 bool_t check_block_impact(char *std_in);
 bool_t check_number(char* input);
-
-option_t process_input(int std_in, char *buffer);
+char *get_input(char *input, int *input_index);
 status_t parse_input(input_t *input, char *buffer);
 
+
+/* IMPLEMENTATION process_commands */
 node_t *check_add_node(input_t *input, node_t *head);
 node_t *check_add_block(input_t *input, node_t *head);
 
 node_t *check_rm_node(input_t *input, node_t *head);
 node_t *check_rm_block(input_t *input, node_t *head);
 
-node_t *process_commands(node_t *head, char *buffer);
+/* PUBLIC */
+void process_commands(input_t *input);
+option_t process_input(int std_in, input_t *input);
 
 #endif
 
