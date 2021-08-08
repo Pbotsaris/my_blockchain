de455636f7d8a2180e167ba68d2b90874c5bd427
khalilmasri <khalil.1996km@gmail.com>
2021-08-08 15:20:02 +0300

    added synced list

--- src/process_input.c
+++ src/process_input.c
@@ -1,32 +1,33 @@
 #include "../include/my_blockchain.h"
 #define BUFF_SIZE 100
 
-option_t basic_commands(char *cmd, node_t *head)
+option_t basic_commands(input_t *input, node_t **synced)
 {
-    if((strcmp(cmd, "sync")) == 0){
-        printf("sync\n");
+    if((strcmp(input->buffer, "sync")) == 0){
+        printf("syncing...\n");
+        *synced = input->unsynced;
         return SYNC;
-    }else if((strcmp(cmd, "quit")) == 0){
+    }else if((strcmp(input->buffer, "quit")) == 0){
         printf("Quiting program successful.\n");
         return QUIT;
-    }else if((strcmp(cmd, "ls")) == 0){
-        print_list(head);
+    }else if((strcmp(input->buffer, "ls")) == 0){
+        print_list(*synced);
         return LS_NID;
-    }else if((strcmp(cmd, "ls -l")) == 0){
-        print_block_list(head);
+    }else if((strcmp(input->buffer, "ls -l")) == 0){
+        print_block_list(*synced);
         return LS_NID_BID;
     }
     return NONE;
 }
 
-option_t process_input(int std_in, input_t *input){
+option_t process_input(int std_in, input_t *input, node_t **synced){
     // check for basic commands
     option_t option = NONE;
     input->buffer = malloc(sizeof(char)*BUFF_SIZE);
     int read_ret = read(std_in, input->buffer, BUFF_SIZE);
     input->buffer[read_ret-1] = '\0';
 
-    option = basic_commands(input->buffer, input->unsynced);
+    option = basic_commands(input, synced);
 
     return option;
 }
