f99c4e6c70e1eeb0a0d8f8a48594ace49892afd1
khalilmasri <khalil.1996km@gmail.com>
2021-08-09 12:40:23 +0300

    added nodes to tail and function copy_list

--- include/list.h
+++ include/list.h
@@ -10,6 +10,7 @@ typedef struct node_s {
 }node_t;
 
 void make_bid_buffer(char *bid, node_t *node);
+node_t *copy_list(node_t *src, node_t *dest);
 node_t *init_list(char *bid, int nid);
 node_t *add_node(node_t *head, char *bid, int nid);
 node_t *add_block(node_t *head, char *bid, int nid);
@@ -24,7 +25,6 @@ void print_list(node_t *head);
 void print_block_list(node_t *head);
 
 void free_list(node_t *head);
-
 
 #endif
 
