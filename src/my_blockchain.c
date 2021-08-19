772059cf9fcb48b50364e5ddaa7f23e90777138f
khalilmasri <khalil.1996km@gmail.com>
2021-08-19 17:20:05 +0300

    added quit

--- src/my_blockchain.c
+++ src/my_blockchain.c
@@ -32,46 +32,48 @@ void free_struct(input_t *input){
 int main(void)
 {
 
-   input_t *input = malloc(sizeof(input_t));
-   input->option = NONE;
-   input->is_synced = FALSE;
-   input->head = NULL;
+    input_t *input = malloc(sizeof(input_t));
+    input->option = NONE;
+    input->is_synced = FALSE;
+    input->head = NULL;
 
-   input->head = get_synced_nodes(input->head);
+    input->head = get_synced_nodes(input->head);
 
-   while(input->option != QUIT){
+    print_message(LAUNCH_MSG);
 
-       prompt_display(input);
-       input->option = process_input(STDIN_FILENO,input); 
+    while(input->option != QUIT){
 
-       switch(input->option){
-           case SYNC:
-               input->head = sync_nodes(input->head);
-               print_message(SYNC_MSG);
-               break;
+        prompt_display(input);
+        input->option = process_input(STDIN_FILENO,input); 
 
-           case LS_NID:
-               print_message(LS_MSG);
-               print_list(input->head);
-               break;
+        switch(input->option){
+            case SYNC:
+                input->head = sync_nodes(input->head);
+                print_message(SYNC_MSG);
+                break;
 
-           case LS_NID_BID:
-               print_message(LS_L_MSG);
-               print_block_list(input->head);
-               break;
+            case LS_NID:
+                print_message(LS_MSG);
+                print_list(input->head);
+                break;
 
-           case QUIT:
+            case LS_NID_BID:
+                print_message(LS_L_MSG);
+                print_block_list(input->head);
+                break;
+
+            case QUIT:
                 write_nodes(input->head);
                 free_struct(input);
                 free_list(input->head);
                 print_message(QUIT_MSG);
                 break;
 
-           default:
-               process_commands(input);
-               break;
-       }
-   } 
+            default:
+                process_commands(input);
+                break;
+        }
+    } 
 
     return 0;
 }
