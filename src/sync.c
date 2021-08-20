37ff65abfb8f41f1fcbfdc89d56448bde2d63d34
Pedro Botsaris <pedro@antfood.com>
2021-08-20 09:17:09 +0200

    fixed read memory leaks

--- src/sync.c
+++ src/sync.c
@@ -5,106 +5,111 @@ node_t * write_blocks(node_t *head, char *bids){
     if(bids[0] == '\0')
         return head;
 
-    int index = 0,
-        i = 0,
-        current = 0;
+    int buff_index = 0,
+        bids_index = 0;
 
-    char *buffer = malloc(sizeof(char*)*MAX_BID_SIZE);
+    char buffer[MAX_BID_SIZE];
 
-    while(bids[current]){
-        if(bids[current] == ' ') continue;
-        
-        if(bids[current] == ','){
-            buffer[index] = '\0';
+    while(bids[bids_index])
+    {
+        if(bids[bids_index] == ' ')
+            continue;
+
+        if(bids[bids_index] == ',')
+        {
+            buffer[buff_index] = '\0';
             head->blocks = add_bid(head->blocks, buffer);
-            i++;
-            index = -1;
+            buff_index = 0;
+        } else {
+            buffer[buff_index] = bids[bids_index];
+            buff_index++;
         }
 
-        buffer[index] = bids[current];
-        current++;
-        index++;
+      bids_index++;
     }
 
     return head;
 }
 
 node_t *get_synced_nodes(node_t *head){
-    
-    int file = open("./bin/saved_nodes.txt", O_RDWR),
-        skip = 0;
 
-    if(file < 0){
-        file = open("./bin/saved_nodes.txt", O_CREAT | O_RDWR);
-        if(file < 0)
-            return NULL;
-    }
+    bool_t     was_nid_read = FALSE;
 
-    char    *current = malloc(sizeof(char)),
-            *nid = malloc(sizeof(char)*BUFSIZ),
-            *bids = malloc(sizeof(char)*BUFSIZ);
-    
-    if(file < 0)
+    int        file,
+               nid_index = 0, 
+               bids_index = 0; 
+    char       current,
+               nid[BUFSIZ],
+               bids[BUFSIZ];
+
+
+    if((file = open("./bin/saved_nodes.txt", O_RDWR)) < 0)
         return NULL;
-    
-    while(read(file, current, 1) > 0){
 
-        if(current[0] == ' ')
+    while(read(file, &current, 1) > 0){
+
+        if(current == ' ')
             continue;
 
-        if(current[0] != ':' && skip == 0)
-            strcat(nid, current);
-        else if(current[0] == ':'){
-            strcat(nid, "\0");
-            skip++;
+        if(current == ':' && (!was_nid_read))
+        {
+            nid[nid_index] = '\0';
+            was_nid_read = TRUE;
             continue;
         }
 
-        if(skip == 1)
-            strcat(bids, current);
+        if(!was_nid_read)
+        {
+            nid[nid_index] = current;
+            nid_index++;
+        }
 
-        if(current[0] == '\n'){
+
+        if(was_nid_read)
+        {
+            bids[bids_index] = current;
+            bids_index++;
+        }
+
+        if(current == '\n'){
+            bids[bids_index] = '\0';
+
             head = add_node(head, atoi(nid));
-            strcat(bids, "\0");
             head = write_blocks(head, bids);
-            skip = 0;
-            nid[0] = '\0';
-            bids[0] = '\0';
+            was_nid_read = FALSE;
+            bids_index = nid_index = 0;
         }
-
     }
-    free(current);
-    free(nid);
-    free(bids);
+
     return head;
 }
 
 void write_nodes(node_t *head){
 
-    int file = open("./bin/saved_nodes.txt", O_RDWR);
+    int file;
+    char nid_str[BUFSIZ];
 
-    if(file < 0){
-        file = open("./bin/saved_nodes.txt", O_CREAT | O_RDWR);
-        if(file < 0)
-            return;
-    }
+    if((file = open("./bin/saved_nodes.txt", O_CREAT | O_RDWR)) < 0)
+        return;
 
-    char *nid_str = malloc(sizeof(char)*BUFSIZ);
+//    char *nid_str = malloc(sizeof(char)*BUFSIZ);
 
-    while(head){
+    while(head)
+    {
         sprintf(nid_str, "%d", head->nid);
         write(file, nid_str, strlen(nid_str));
         write(file, ": ", strlen(": "));
-        for(int i = 0; head->blocks->bids[i][0]; i++){
+
+        for(int i = 0; i < head->blocks->index; i++){
+
             write(file, head->blocks->bids[i], strlen(head->blocks->bids[i]));
             write(file, ", ", strlen(", "));
+
         }
         write(file, "\n", strlen("\n"));
 
         head = head->next;
     }
-
-    free(nid_str);
 }
 
 node_t* impact_all_nodes(node_t *head){
@@ -115,7 +120,7 @@ node_t* impact_all_nodes(node_t *head){
         for(int i = 0; head->blocks->bids[i][0]; i++)
             if(!(bid_exists(current->blocks, head->blocks->bids[i])))
                 current->blocks = add_bid(current->blocks, head->blocks->bids[i]); 
-        
+
         current = current->next;
     }
 
@@ -125,7 +130,7 @@ node_t* impact_all_nodes(node_t *head){
 node_t* sync_nodes(node_t *head){
 
     node_t *current = head->next;
-    
+
     while(current){
         for(int i = 0; current->blocks->bids[i][0] != '\0'; i++){
             if(!(bid_exists(head->blocks, current->blocks->bids[i]))){
